5eee5b1b7bb37c59f35e063421a6f6620e1c9580
Francisco Marcano <fjmn2001@gmail.com>
2025-02-16 03:22:13 -0400

    adding swagger support v2

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -65,7 +65,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): JsonResponse
     {
-        $request->user()->currentAccessToken()->delete();
+        if(method_exists($request->user()->currentAccessToken(), 'delete')) {
+            $request->user()->currentAccessToken()->delete();
+        } else {
+            auth()->guard('web')->logout();
+        }
 
         return response()->json(['message' => 'Logged out']);
     }
