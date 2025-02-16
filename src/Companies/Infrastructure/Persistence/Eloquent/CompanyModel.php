882f3220bf859a81d998484a6c153316ed4dc69e
Francisco Marcano <fjmn2001@gmail.com>
2025-02-16 18:14:04 -0400

    adding tenat model

---
+++ src/Companies/Infrastructure/Persistence/Eloquent/CompanyModel.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MedineTech\Companies\Infrastructure\Persistence\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class CompanyModel extends Model
+{
+
+}
