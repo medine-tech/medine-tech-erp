42a837c6aa8afce4b53f3e79d48f8999fbb3ea69
luiscggonzalez <redes.luisgonzalez@gmail.com>
2025-02-20 18:38:58 -0400

    feat(ME-81): Refactory code Dashboard

--- apps/backoffice/src/app/dashboard/page.tsx
+++ apps/backoffice/src/app/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function DashboardPage() {
             <DashboardHeader />
             <main className="flex-1 space-y-4 p-8 pt-6">
                 <div className="flex items-center justify-between">
-                    <h2 className="text-3xl font-bold tracking-tight">Your Companies</h2>
+                    <h2 className="text-3xl font-bold tracking-tight">Tus Empresas</h2>
                 </div>
                 <CompanyList companies={mockCompanies} />
             </main>
