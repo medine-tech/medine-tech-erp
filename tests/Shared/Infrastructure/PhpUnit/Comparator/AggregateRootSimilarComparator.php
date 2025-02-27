14f68a2aac0130e6e5241ef036e7e7d8bd205e62
Francisco Marcano <fjmn2001@gmail.com>
2025-02-27 09:11:59 -0400

    chore: adding unit test case environment

---
+++ tests/Shared/Infrastructure/PhpUnit/Comparator/AggregateRootSimilarComparator.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Shared\Infrastructure\PhpUnit\Comparator;
+
+use MedineTech\Shared\Domain\Aggregate\AggregateRoot;
+use PHPUnit\Util\Exporter;
+use ReflectionException;
+use ReflectionObject;
+use SebastianBergmann\Comparator\Comparator;
+use SebastianBergmann\Comparator\ComparisonFailure;
+use Tests\Shared\Domain\TestUtils;
+
+final class AggregateRootSimilarComparator extends Comparator
+{
+    public function accepts($expected, $actual): bool
+    {
+        $aggregateRootClass = AggregateRoot::class;
+
+        return $expected instanceof $aggregateRootClass && $actual instanceof $aggregateRootClass;
+    }
+
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false): void
+    {
+        $actualEntity = clone $actual;
+        $actualEntity->pullDomainEvents();
+
+        if (!$this->aggregateRootsAreSimilar($expected, $actualEntity)) {
+            throw new ComparisonFailure(
+                $expected,
+                $actual,
+                Exporter::export($expected),
+                Exporter::export($actual),
+                'Failed asserting the aggregate roots are equal.'
+            );
+        }
+    }
+
+    private function aggregateRootsAreSimilar(AggregateRoot $expected, AggregateRoot $actual): bool
+    {
+        if (!$this->aggregateRootsAreTheSameClass($expected, $actual)) {
+            return false;
+        }
+
+        return $this->aggregateRootPropertiesAreSimilar($expected, $actual);
+    }
+
+    private function aggregateRootsAreTheSameClass(AggregateRoot $expected, AggregateRoot $actual): bool
+    {
+        return $expected::class === $actual::class;
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function aggregateRootPropertiesAreSimilar(AggregateRoot $expected, AggregateRoot $actual): bool
+    {
+        $expectedReflected = new ReflectionObject($expected);
+        $actualReflected = new ReflectionObject($actual);
+
+        foreach ($expectedReflected->getProperties() as $expectedReflectedProperty) {
+            $actualReflectedProperty = $actualReflected->getProperty($expectedReflectedProperty->getName());
+
+            $expectedReflectedProperty->setAccessible(true);
+            $actualReflectedProperty->setAccessible(true);
+
+            $expectedProperty = $expectedReflectedProperty->getValue($expected);
+            $actualProperty = $actualReflectedProperty->getValue($actual);
+
+            if (!TestUtils::isSimilar($expectedProperty, $actualProperty)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/Shared/Infrastructure/PhpUnit/Comparator/DomainEventArraySimilarComparator.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Shared\Infrastructure\PhpUnit\Comparator;
+
+use MedineTech\Shared\Domain\Bus\Event\DomainEvent;
+use Tests\Shared\Domain\TestUtils;
+use PHPUnit\Util\Exporter;
+use SebastianBergmann\Comparator\Comparator;
+use SebastianBergmann\Comparator\ComparisonFailure;
+use function Lambdish\Phunctional\all;
+use function Lambdish\Phunctional\any;
+use function Lambdish\Phunctional\instance_of;
+
+final class DomainEventArraySimilarComparator extends Comparator
+{
+    public function accepts($expected, $actual): bool
+    {
+        return is_array($expected)
+            && is_array($actual)
+            && (all(instance_of(DomainEvent::class), $expected)
+                && all(instance_of(DomainEvent::class), $actual));
+    }
+
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false): void
+    {
+        if (!$this->contains($expected, $actual) || count($expected) !== count($actual)) {
+            throw new ComparisonFailure(
+                $expected,
+                $actual,
+                Exporter::export($expected),
+                Exporter::export($actual),
+                'Failed asserting the collection of Events contains all the expected elements.'
+            );
+        }
+    }
+
+    private function contains(array $expectedArray, array $actualArray): bool
+    {
+        $exists = static fn(DomainEvent $expected): bool => any(
+            static fn(DomainEvent $actual): bool => TestUtils::isSimilar($expected, $actual),
+            $actualArray
+        );
+
+        return all($exists, $expectedArray);
+    }
+}
