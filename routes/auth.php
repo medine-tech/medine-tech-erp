5a6ab4d99cabed89c0173bfa41400a8e3d16f6fa
juan <juanvillarroelcontacto@gmail.com>
2025-03-01 19:11:40 -0400

    Fix(ME-84): auth correction

--- routes/auth.php
+++ routes/auth.php
@@ -25,6 +25,7 @@ Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
     ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
